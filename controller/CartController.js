bbbb5db66bd1ede39601c268341991c904ca790a
KokilaDev <kokiladewmini527@gmail.com>
2026-05-05 11:30:11 +0530

    load items

--- controller/CartController.js
+++ controller/CartController.js
@@ -1,5 +1,6 @@
 import { CartModel } from "../model/CartModel.js";
 import { CustomerModel } from "../model/CustomerModel.js";
+import { ItemModel } from "../model/ItemModel.js";
 
 $(document).ready(function () {
     $('#orderId').text(CartModel.generateOrderId());
@@ -7,9 +8,14 @@ $(document).ready(function () {
     $('#orderDate').text(CartModel.updateOrderDate());
 
     loadCustomers();
+    loadItems();
 
     $(document).on("customerAdded customerUpdated customerDeleted", function () {
         loadCustomers();
+    });
+
+    $(document).on("itemAdded itemUpdated itemDeleted", function () {
+        loadItems();
     });
 });
 
@@ -19,5 +25,14 @@ function loadCustomers() {
     select.append(`<option value="" disabled selected hidden>Select Customer</option>`);
     CustomerModel.getAllCustomers().forEach(customer => {
         select.append(`<option value="${customer._customer_id}">${customer._customer_name}</option>`);
+    });
+}
+
+function loadItems() {
+    const select = $('#selectItem');
+    select.empty();
+    select.append(`<option value="" disabled selected hidden>Select Item</option>`);
+    ItemModel.getAllItems().forEach(item => {
+        select.append(`<option value="${item._item_code}">${item._item_name}</option>`);
     });
 }
