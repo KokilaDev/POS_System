a2da67ea44fe45f86998c4962fbe9cdc1f128205
KokilaDev <kokiladewmini527@gmail.com>
2026-05-05 11:08:44 +0530

    load orderDate

--- controller/CartController.js
+++ controller/CartController.js
@@ -2,4 +2,5 @@ import { CartModel } from "../model/CartModel.js";
 
 $(document).ready(function () {
     $('#orderId').text(CartModel.generateOrderId());
+    $('#orderDate').text(CartModel.updateOrderDate());
 });
--- model/CartModel.js
+++ model/CartModel.js
@@ -6,5 +6,15 @@ export const CartModel = {
         let lastId = order_db[order_db.length - 1].order_id;
         let num = parseInt(lastId.substring(1)) + 1;
         return "O" + String(num).padStart(3, "0");
+    },
+
+    updateOrderDate() {
+        let today = new Date();
+
+        let year = today.getFullYear();
+        let month = String(today.getMonth() + 1).padStart(2, '0');
+        let day = String(today.getDate()).padStart(2, '0');
+
+        return `${year}-${month}-${day}`;
     }
 }
